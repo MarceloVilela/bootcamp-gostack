591724a70f6bee1909acbb493705513682a7a796
Marcelo Vilela da Silva <marcelo.vilela.s@gmail.com>
2019-10-26 14:31:41 -0300

    modulo05

---
+++ modulo05/src/routes.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
+
+import Main from './pages/Main'
+import Repository from './pages/Repository'
+
+// import { Container } from './styles';
+
+export default function Routes() {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" exact component={Main}></Route>
+        <Route path="/repository/:repository" component={Repository}></Route>
+      </Switch>
+    </BrowserRouter>
+  );
+}
