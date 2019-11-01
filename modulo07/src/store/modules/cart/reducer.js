c1f94a4643f68d97954908b6d103641ceb7a59b5
Marcelo Vilela da Silva <marcelo.vilela.s@gmail.com>
2019-11-01 14:41:29 -0300

    modulo07 - redux / Produto duplicado

--- modulo07/src/store/modules/cart/reducer.js
+++ modulo07/src/store/modules/cart/reducer.js
@@ -1,9 +1,29 @@
-export default function cart(state = [], action) {
-  console.log(state);
+import produce from 'immer';
 
+export default function cart(state = [], action) {
   switch (action.type) {
     case 'ADD_TO_CART':
-      return [...state, action.product];
+      return produce(state, draft => {
+        const productIndex = draft.findIndex(p => p.id === action.product.id);
+
+        if (productIndex >= 0) {
+          draft[productIndex].ammount += 1;
+        } else {
+          draft.push({
+            ...action.product,
+            ammount: 1,
+          });
+        }
+      });
+    /*
+      return [
+        ...state,
+        {
+          ...action.product,
+          ammount: 1,
+        },
+      ];
+      */
     default:
       return state;
   }
