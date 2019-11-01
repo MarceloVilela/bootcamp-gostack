2d2674f49b6f7247ce50efcd29f0d7455a809035
Marcelo Vilela da Silva <marcelo.vilela.s@gmail.com>
2019-11-01 17:09:34 -0300

    modulo07 - redux / Saga

--- modulo07/src/store/index.js
+++ modulo07/src/store/index.js
@@ -1,10 +1,21 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import createSagaMiddleware from 'redux-saga';
 
 import rootReducer from './modules/rootReducer';
+import rootSaga from './modules/rootSaga';
+
+const sagaMiddleware = createSagaMiddleware();
 
 const dev = process.env.NODE_ENV === 'development';
-const enhancer = dev ? console.tron.createEnhancer() : null;
+const enhancer = dev
+  ? compose(
+      console.tron.createEnhancer(),
+      applyMiddleware(sagaMiddleware)
+    )
+  : applyMiddleware(sagaMiddleware);
 
 const store = createStore(rootReducer, enhancer);
+
+sagaMiddleware.run(rootSaga);
 
 export default store;
