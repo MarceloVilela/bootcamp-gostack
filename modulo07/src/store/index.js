9c8bb89976a2cbcefefc05151d93a1e00f180667
Marcelo Vilela da Silva <marcelo.vilela.s@gmail.com>
2019-11-01 14:19:12 -0300

    modulo07 - redux / reactotron

--- modulo07/src/store/index.js
+++ modulo07/src/store/index.js
@@ -2,6 +2,9 @@ import { createStore } from 'redux';
 
 import rootReducer from './modules/rootReducer';
 
-const store = createStore(rootReducer);
+const dev = process.env.NODE_ENV === 'development';
+const enhancer = dev ? console.tron.createEnhancer() : null;
+
+const store = createStore(rootReducer, enhancer);
 
 export default store;
