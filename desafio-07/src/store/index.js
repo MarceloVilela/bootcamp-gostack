5a4df9249451601778a654897b7c3fd734397d23
Marcelo Vilela da Silva <marcelo.vilela.s@gmail.com>
2019-11-02 10:54:19 -0300

    Utiliza diretorio src da aplicacao web - modulo07

---
+++ desafio-07/src/store/index.js
@@ -0,0 +1,26 @@
+import { createStore, applyMiddleware, compose } from 'redux';
+import createSagaMiddleware from 'redux-saga';
+
+import rootReducer from './modules/rootReducer';
+import rootSaga from './modules/rootSaga';
+
+const dev = process.env.NODE_ENV === 'development';
+
+const sagaMonitor = dev
+  ? console.tron.createSagaMonitor()
+  : null;
+
+const sagaMiddleware = createSagaMiddleware({sagaMonitor});
+
+const enhancer = dev
+  ? compose(
+      console.tron.createEnhancer(),
+      applyMiddleware(sagaMiddleware)
+    )
+  : applyMiddleware(sagaMiddleware);
+
+const store = createStore(rootReducer, enhancer);
+
+sagaMiddleware.run(rootSaga);
+
+export default store;
