34269aee63b7f57d3c030d518b2207c000f29189
Marcelo Vilela da Silva <marcelo.vilela.s@gmail.com>
2019-11-09 09:03:27 -0300

    Retornos da api - infos necessarias para o frontend (modulo09)

--- modulo03/src/app/controllers/UserController.js
+++ modulo03/src/app/controllers/UserController.js
@@ -1,5 +1,6 @@
 import * as Yup from 'yup'
 import User from '../models/User'
+import File from '../models/File'
 
 class UserController {
   async store(req, res) {
@@ -43,7 +44,7 @@ class UserController {
         .when('oldPassword', (oldPassword, field) => {
           oldPassword ? field.required().oneOf([Yup.ref('password')]) : field
         }),
-      confirmPassword: Yup.string().when('password', (password, field) => 
+      confirmPassword: Yup.string().when('password', (password, field) =>
         password ? field.required().oneOf([Yup.ref('password')]) : field
       )
     })
@@ -68,13 +69,24 @@ class UserController {
       return res.status(401).json({ error: 'Password does not match' })
     }
 
-    const { id, name, provider } = await user.update(req.body)
+    await user.update(req.body)
+
+    const { id, name, provider, avatar } = await User.findByPk(req.userId, {
+      include: [
+        {
+          model: File,
+          as: 'avatar',
+          attributes: ['id', 'path', 'url'],
+        }
+      ]
+    })
 
     return res.json({
       id,
       name,
       email,
       provider,
+      avatar
     })
   }
 }
